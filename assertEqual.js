51b72f2a8f8c9bce430f5180481a3dbe17276e68
Kino Bonilla <kinobonilla@gmail.com>
2021-11-22 23:29:38 +0000

    Updated output strings using template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,14 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('✅ Assertion Passed: ' + actual + ' == ' + expected + ' ✅');
+    console.log(`✅ Assertion Passed: ${actual} == ${expected} ✅`);
   } else {
-    console.log('❌ Assertion Failed: ' + actual + ' !== ' + expected + ' ❌');
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected} ❌`);
   }
 };
 
 // TEST CODE
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
 console.log(assertEqual(1, 1));
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
 console.log(assertEqual('Hello', 'Hello'));
 console.log(assertEqual('Monday', 'Tuesday'));
 console.log(assertEqual(22, 22));
