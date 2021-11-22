31a112cedb8c6076df79927bac9a8573567d337f
Kino Bonilla <kinobonilla@gmail.com>
2021-11-22 22:28:52 +0000

    Assertion function which compares two values

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅ Assertion Passed: ' + actual + ' == ' + expected + ' ✅');
+  } else {
+    console.log('❌ Assertion Failed: ' + actual + ' !== ' + expected + ' ❌');
+  }
+};
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual('Hello', 'Hello'));
+console.log(assertEqual('Monday', 'Tuesday'));
+console.log(assertEqual(22, 22));
+console.log(assertEqual(16, 61));
