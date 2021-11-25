65be8f6657135e7f098ea40796a8af2393bc0999
Kino Bonilla <kinobonilla@gmail.com>
2021-11-25 23:52:41 +0000

    Added assertion function and test cases

--- map.js
+++ map.js
@@ -1,5 +1,18 @@
 // map function will return a new array based on the results of the callback function
 
+/*
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((num, i) => num === arr2[i]);
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  console.log(eqArrays(arr1, arr2) === true ? `✅ Assertion Passed ✅` : `❌ Assertion Failed ❌`);
+};
+*/
+
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
@@ -15,3 +28,13 @@ const firstLetter = map(words, word => word[0]);
 // test
 console.log(firstLetter);
 
+// assertion test 
+/*
+const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word.length);
+const results3 = map(words, word => word.length * 2);
+
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(results2, [6, 7, 2, 5, 3]);
+assertArraysEqual(results3, [12, 14, 4, 10, 6]);
+*/
