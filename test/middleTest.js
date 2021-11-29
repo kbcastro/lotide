e0819bae15d5a11d381b70adf47cd9b8ae45aa87
Kino Bonilla <kinobonilla@gmail.com>
2021-11-29 20:23:42 +0000

    Assertion test file for middle.js

---
+++ test/middleTest.js
@@ -0,0 +1,18 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+// TEST
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+assertArraysEqual(middle([1, 2, 3]), ["2"]); // false
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // true
+assertArraysEqual(middle([1, 2, 3, 4]), [3]); // false
+assertArraysEqual(middle([1, 2]), []); // true
+assertArraysEqual(middle([1]), [1]); // false
+assertArraysEqual(middle([1, 2, 3]), [2]); // true
