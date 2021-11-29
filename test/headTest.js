4c216c288ef4efdf18448a2b3318931aa4d28d22
Kino Bonilla <kinobonilla@gmail.com>
2021-11-29 20:53:56 +0000

    Updated assertions using Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,23 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-// TEST CODE
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-console.log(assertEqual(head(['Apple']), 'Apple'));
-console.log(assertEqual(head([9]), 6));
-console.log(assertEqual(head([]), 'Goodbye'));
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['6']), '5'); 
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello'); 
+  });
+  it("returns 'Apple' for ['Apple']", () => {
+    assert.strictEqual(head(['Apple']), 'Apple'); 
+  });
+  it("returns 9 for [9]", () => {
+    assert.strictEqual(head([9]), 9); 
+  });
+  it("returns 'Goodbye' for ['Goodbye']", () => {
+    assert.strictEqual(head([]), 'Goodbye'); 
+  });
+});
